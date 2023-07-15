147df5c2d0e4dfc7cd91a28b96db197d56b61688
JunRain <sa2313@tukorea.ac.kr>
2023-07-15 16:16:58 +0900

    first-setting

---
+++ src/test/java/com/umc/con/ConApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.con;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
