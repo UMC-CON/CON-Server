7debaf15ff83a1cffb1260b7fda27a23c37c3ab3
ysndy <ysndy@naver.com>
2023-07-20 17:37:07 +0900

    [FEAT] #4 필요 없는 부분 삭제

--- src/main/java/com/umc/con/platform/domain/repository/PlatformRepository.java
+++
@@ -1,8 +0,0 @@
-package com.umc.con.platform.domain.repository;
-
-
-import com.umc.con.platform.domain.entity.Platform;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlatformRepository extends JpaRepository<Platform, Integer> {
-}
