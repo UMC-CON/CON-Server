5f82e2a16bcf4528635c9a03322ed5b4daded6e5
JunRain <sa2313@tukorea.ac.kr>
2023-07-23 23:59:00 +0900

    [FEAT]#9 DB에서 유저를 가져와 Security.User로 변환하는 Service 구현

---
+++ src/main/java/com/umc/cons/common/login/service/LoginService.java
@@ -0,0 +1,29 @@
+package com.umc.cons.common.login.service;
+
+import com.umc.cons.member.domain.entity.Member;
+import com.umc.cons.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow();
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().name())
+                .build();
+    }
+}
