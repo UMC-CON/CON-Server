105422efb9476e763601e7b3be02afe5351a80c1
JEE YONG JEONG <129194852+jeeyong01@users.noreply.github.com>
2023-08-06 22:25:55 +0900

    [FIX] #3 RecordController uri매핑 관련 수정

--- src/main/java/com/umc/cons/record/controller/RecordController.java
+++ src/main/java/com/umc/cons/record/controller/RecordController.java
@@ -2,6 +2,8 @@ package com.umc.cons.record.controller;
 
 import com.umc.cons.common.config.BaseResponse;
 import com.umc.cons.common.config.BaseResponseStatus;
+import com.umc.cons.post.domain.entity.Post;
+import com.umc.cons.post.service.PostService;
 import com.umc.cons.record.domain.entity.Record;
 import com.umc.cons.record.dto.RecordDTO;
 import com.umc.cons.record.dto.RecordResponseDTO;
@@ -17,12 +19,13 @@ import static com.umc.cons.common.config.BaseResponseStatus.RESPONSE_ERROR;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/app/post")
+@RequestMapping("/app/post/{postId}/record")
 public class RecordController {
     private final RecordService recordService;
+    private final PostService postService;
 
     // 기록 추가
-    @PostMapping("/{postId}/record")
+    @PostMapping
     public BaseResponse<BaseResponseStatus> addRecords(@PathVariable Long postId, @RequestBody List<RecordDTO> recordDTOList){
         try{
             List<Record> records = recordService.createRecords(postId, recordDTOList);
@@ -36,9 +39,10 @@ public class RecordController {
     }
 
     // 기록 수정
-    @PatchMapping("/record/{id}")
-    public BaseResponse<BaseResponseStatus> updateRecord(@PathVariable Long id, @RequestBody RecordDTO recordDTO){
+    @PatchMapping("/{id}")
+    public BaseResponse<BaseResponseStatus> updateRecord(@PathVariable Long postId, @PathVariable Long id, @RequestBody RecordDTO recordDTO){
         try{
+            Post post = postService.findByIdAndNotDeleted(postId);
             Record updatedRecord = recordService.updateRecord(id, recordDTO);
             if (updatedRecord == null) {
                 return new BaseResponse<>(REQUEST_ERROR);
@@ -50,9 +54,10 @@ public class RecordController {
     }
 
     // 기록 삭제
-    @DeleteMapping("/record/{id}")
-    public BaseResponse<BaseResponseStatus> deleteRecord(@PathVariable Long id){
+    @DeleteMapping("/{id}")
+    public BaseResponse<BaseResponseStatus> deleteRecord(@PathVariable Long postId, @PathVariable Long id){
         try{
+            Post post = postService.findByIdAndNotDeleted(postId);
             recordService.deleteRecord(id);
             return new BaseResponse<>(SUCCESS);
         } catch (IllegalArgumentException e){
@@ -61,7 +66,7 @@ public class RecordController {
     }
 
     // 기록 조회
-    @GetMapping("/{postId}/record")
+    @GetMapping
     public BaseResponse<List<RecordResponseDTO>> findAllRecord(@PathVariable Long postId){
         try{
             List<Record> records = recordService.findAllRecord(postId)
