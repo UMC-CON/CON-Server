c59679b90757f6454095790488afdf93ec742a16
JEE YONG JEONG <129194852+jeeyong01@users.noreply.github.com>
2023-08-09 00:23:50 +0900

    [FIX] #3 로직 변경으로 인한 post entity content, imageurl 추가 및 수정

--- src/main/java/com/umc/cons/post/domain/entity/Post.java
+++ src/main/java/com/umc/cons/post/domain/entity/Post.java
@@ -2,7 +2,6 @@ package com.umc.cons.post.domain.entity;
 
 import com.umc.cons.common.util.BaseTimeEntity;
 import com.umc.cons.post.dto.PostDTO;
-import com.umc.cons.record.domain.entity.Record;
 import com.umc.cons.share.domain.entity.Share;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +10,6 @@ import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Max;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity(name = "post")
 @Getter
@@ -32,13 +29,16 @@ public class Post extends BaseTimeEntity {
     @Column(nullable = false, name = "title", length = 10)
     private String title;
 
-    @Column(name = "image_url")
-    private String imageUrl;
-
     @Column(nullable = false, name = "score")
     @Min(value = 0, message = "점수는 0보다 크거나 같아야 합니다.")
     @Max(value = 5, message = "점수는 5보다 작거나 같아야 합니다.")
     private double score;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(nullable = false, name = "content", columnDefinition = "CLOB")
+    private String content;
 
     @Column(nullable = false, name = "is_deleted")
     private boolean isDeleted = false;
@@ -46,19 +46,11 @@ public class Post extends BaseTimeEntity {
     @OneToOne(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private Share share;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Record> records = new ArrayList<>();
-
-    public void addRecord(Record record) {
-        records.add(record);
-        record.setPost(this);
-    }
-
     public void update(PostDTO postDTO){
         this.memberId = postDTO.getMemberId();
         this.contentId = postDTO.getContentId();
         this.title = postDTO.getTitle();
-        this.imageUrl = postDTO.getImageUrl();
         this.score = postDTO.getScore();
+        this.content = postDTO.getContent();
     }
 }
