2abb9cff03ad1e1de7aae088038c6ac882927b3e
JEE YONG JEONG <129194852+jeeyong01@users.noreply.github.com>
2023-08-09 00:32:58 +0900

    [FIX] #3 로직 변경으로 인한 DTO 수정

--- src/main/java/com/umc/cons/post/dto/PostResponseDTO.java
+++ src/main/java/com/umc/cons/post/dto/PostResponseDTO.java
@@ -1,15 +1,11 @@
 package com.umc.cons.post.dto;
 
 import com.umc.cons.post.domain.entity.Post;
-import com.umc.cons.record.domain.entity.Record;
-import com.umc.cons.record.dto.RecordResponseDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
@@ -19,24 +15,15 @@ public class PostResponseDTO {
     private LocalDate createdAt;
     private double score;
     private String imageUrl;
-    private List<RecordResponseDTO> recordList;
+    private String content;
 
     public static PostResponseDTO convertToResponseDTO(Post post){
         PostResponseDTO responseDTO = new PostResponseDTO();
         responseDTO.setTitle(post.getTitle());
-        responseDTO.setImageUrl(post.getImageUrl());
         responseDTO.setScore(post.getScore());
         responseDTO.setCreatedAt(post.getCreatedAt().toLocalDate());
-
-        List<Record> records = post.getRecords();
-        List<RecordResponseDTO> recordDTOList = new ArrayList<>();
-
-        for(Record record : records){
-            RecordResponseDTO recordDTO = RecordResponseDTO.of(record);
-            recordDTOList.add(recordDTO);
-        }
-
-        responseDTO.setRecordList(recordDTOList);
+        responseDTO.setImageUrl(post.getImageUrl());
+        responseDTO.setContent(post.getContent());
 
         return responseDTO;
     }
