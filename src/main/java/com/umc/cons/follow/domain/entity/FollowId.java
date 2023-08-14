b1ddc8cda8f950842dc7cb44bf85b143fc6daa8b
HanSeongLEE <leehs98712@gmail.com>
2023-08-14 17:57:34 +0900

    [FEAT] #12 Member Entity와 mapping관계 추가

--- src/main/java/com/umc/cons/follow/domain/entity/FollowId.java
+++ src/main/java/com/umc/cons/follow/domain/entity/FollowId.java
@@ -2,17 +2,30 @@ package com.umc.cons.follow.domain.entity;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Embeddable
 @Data
 @NoArgsConstructor
 public class FollowId implements Serializable {
-    @Column(name = "follower_id")
+    //for testing
+    @Column(name = "follower_id", nullable = false)
     private Long followerId;
-    @Column(name = "following_id")
+    @Column(name = "following_id", nullable = false)
     private Long followingId;
+
+    // 실제 mapping관계 반영
+
+    /*
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id")
+    //follower.getId()로 메서드 변경 필요
+    private Member follower;
+    //
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_id")
+    private Member following;
+    */
+
 }
