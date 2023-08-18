74cc9f51875cf563bde4ae6b26130e5f3c3a0645
JunRain <sa2313@tukorea.ac.kr>
2023-08-18 17:04:25 +0900

    [FIX] 프로필 수정에서 이미지 받아올 수 있도록 수정

--- src/main/java/com/umc/cons/member/dto/ProfileRequestDto.java
+++ src/main/java/com/umc/cons/member/dto/ProfileRequestDto.java
@@ -1,5 +1,7 @@
 package com.umc.cons.member.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ProfileRequestDto {
-	private String imageUrl;
+	private MultipartFile imageFile;
 	private String name;
 	private String introduction;
 }
