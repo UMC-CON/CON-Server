7887e56ba6b55ae24fea188be04d789739f34b00
JunRain <sa2313@tukorea.ac.kr>
2023-08-18 17:40:08 +0900

    [FIX] DTO에 @Setter 추가

--- src/main/java/com/umc/cons/member/dto/ProfileRequestDto.java
+++ src/main/java/com/umc/cons/member/dto/ProfileRequestDto.java
@@ -5,8 +5,10 @@ import org.springframework.web.multipart.MultipartFile;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ProfileRequestDto {
 	private MultipartFile imageFile;
